f3ab459c667a5fc687ae4d6bece16ad915ad67da
shazzy152 <44363342+shazzy152@users.noreply.github.com>
2021-02-17 11:36:07 +0530

    updated to functional components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,38 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-class App extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            robots: [],
-            searchfield : ''            
-        }
-    }
+function App() {
 
-    componentDidMount() {
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
+
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response =>response.json())
-        .then(users => this.setState({robots: users}));
+        .then(users => setRobots(users));
+    },[]) 
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
-
-    render() {
-        const { robots, searchfield } = this.state;
         const filteredRobot = robots.filter(robot =>{
             return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         });
         return(
             <div className="tc">
                 <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
+                <SearchBox searchChange={onSearchChange} />
                 <Scroll>
                     <ErrorBoundry>
                         <CardList robots={filteredRobot} />
@@ -41,6 +35,5 @@ class App extends React.Component {
             </div>
                 );
     }
-}
 
 export default App;
